1db5b362f08c4bb9b3673670d7f0c8ac06c6c8f3
harshthakur066 <harshthakur066@gmail.com>
2020-03-22 20:11:15 +0530

    updated data

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { uuid } from 'uuidv4';
 
 import Header from './components/header';
 
 const App = () => {
+
+  const [items, setItems] = useState([
+    { id: 1, text: 'Milk' },
+    { id: 2, text: 'Eggs' },
+    { id: 3, text: 'Breads' },
+    { id: 4, text: 'Apples' },
+  ])
+
   return (
     <View style={styles.container}>
-      <Header />
+      <Header title='Shopping List' />
+      <FlatList
+        data={items}
+        renderItem={
+          ({ item }) => <Text> {item.text} </Text>
+        }
+      />
     </View>
   );
 };
